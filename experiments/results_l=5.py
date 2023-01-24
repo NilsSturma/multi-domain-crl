23d6a53a6ccf058d4a004aadd0b67e57c434535c
Nils Sturma <nils.sturma@web.de>
2023-01-24 14:23:21 +0100

    code to plot results

---
+++ experiments/results_l=5.py
@@ -0,0 +1,43 @@
+# ===== IMPORTS =====
+import pickle
+import numpy as np
+from time import time
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy.stats import beta, uniform, expon, lognorm, weibull_min, chi2, t, gumbel_r, skewnorm
+from utils import get_statistics, plot
+
+
+# ===== LOAD =====
+l = 5
+d = 48
+
+with open(f"results-paper/nr_doms=2_l={l}_d={d}.pkl", "rb") as f:
+    info2 = pickle.load(f)
+with open(f"results-paper/nr_doms=3_l={l}_d={d}.pkl", "rb") as f:
+    info3 = pickle.load(f)
+with open(f"results-paper/nr_doms=4_l={l}_d={d}.pkl", "rb") as f:
+    info4 = pickle.load(f)
+
+# ===== GET STATISTICS =====
+nsamples_list = info2["metadata"]["nsamples_list"]
+avg_number_shared2, too_many_shared_rate2, mixing_error_rate2, graph_error_median2 = get_statistics(info2)
+avg_number_shared3, too_many_shared_rate3, mixing_error_rate3, graph_error_median3 = get_statistics(info3)
+avg_number_shared4, too_many_shared_rate4, mixing_error_rate4, graph_error_median4 = get_statistics(info3)
+
+# ===== PLOT =====
+plot(nsamples_list, avg_number_shared2, avg_number_shared3, avg_number_shared4,
+                ylabel="Average of $\hat{\ell}$",
+                path=f"results-paper/avg-shared-nodes_l={l}_d={d}.png", ylim=(-0.1,3.1))
+
+plot(nsamples_list, too_many_shared_rate2, too_many_shared_rate3, too_many_shared_rate4,
+                ylabel="Fraction with $\hat{\ell} > \ell$",
+                path=f"results-paper/too-many-shared-nodes_l={l}_d={d}.png")
+
+plot(nsamples_list, mixing_error_rate2, mixing_error_rate3, mixing_error_rate4,
+                ylabel="score$_B$",
+                path=f"results-paper/mixing-error_l={l}_d={d}.png")
+
+plot(nsamples_list, graph_error_median2, graph_error_median3, graph_error_median4,
+                ylabel="score$_A$",
+                path=f"results-paper/graph-error_l={l}_d={d}.png")
--- experiments/utils.py
+++ experiments/utils.py
@@ -1,4 +1,5 @@
 import numpy as np
+import seaborn as sns
 import matplotlib.pyplot as plt
 
 def sample_normed(n, rv):
@@ -35,3 +36,52 @@ def plot_hist_noise(indep_comps, save=False, name="plot.png", figsize=(14,9)):
         fig.savefig(name)
     else:
         plt.show()
+
+def get_statistics(info):
+    metadata = info["metadata"]
+    nexp = metadata["nexp"]
+    nsamples_list = metadata["nsamples_list"]
+    m = metadata["model_specs"]
+
+    mixing_error_median = np.zeros(len(nsamples_list))
+    graph_error_median = np.zeros(len(nsamples_list))
+    too_many_shared_rate = np.zeros(len(nsamples_list))
+    avg_number_shared = np.zeros(len(nsamples_list))
+    for s_ix, n in enumerate(nsamples_list):
+        mixing_errors = np.full(nexp, np.nan)
+        graph_errors = np.full(nexp, np.nan)
+        too_many_shared = np.full(nexp, False)
+        number_shared = np.full(nexp, 0)
+        for exp_ix in range(nexp):
+            number_shared[exp_ix] = info["results"][(s_ix, exp_ix)]["nr_joint"]
+            mixing_errors[exp_ix] = info["results"][(s_ix, exp_ix)]["mixing_error"] / \
+                (min(number_shared[exp_ix], len(m["joint_idx"])) * sum(m["dims"])) ### normalizing!!
+            if number_shared[exp_ix] > len(m["joint_idx"]):
+                too_many_shared[exp_ix] = True
+            if number_shared[exp_ix] == len(m["joint_idx"]):
+                graph_errors[exp_ix] = info["results"][(s_ix, exp_ix)]["graph_error"] / \
+                    (len(m["joint_idx"])) ### normalizing!!
+        too_many_shared_rate[s_ix] = too_many_shared.mean()
+        mixing_error_median[s_ix] = np.nanmedian(mixing_errors)
+        graph_error_median[s_ix] = np.nanmedian(graph_errors)
+        avg_number_shared[s_ix] = number_shared.mean()
+    return (avg_number_shared, too_many_shared_rate, mixing_error_median, graph_error_median)
+
+def plot(nsamples_list, stats2, stats3, stats4, ylabel="Score", path="test.png", fontsize=13, ylim=None):
+    sns.set()
+    plt.figure(figsize=(3.3,2.7))
+    plt.clf()
+    if ylim is not None:
+        plt.ylim(ylim)
+    plt.plot(nsamples_list, stats2, "-", color="blue", label="2 domains")
+    plt.plot(nsamples_list, stats3, "--", color="red", label="3 domains")
+    plt.plot(nsamples_list, stats4, "-.", color="green", label="4 domains")
+    plt.xscale("log")
+    plt.xlabel("Sample size", fontsize=fontsize)
+    plt.ylabel(ylabel, fontsize=fontsize)
+    plt.legend(fontsize=fontsize)
+    plt.xticks(fontsize=fontsize)
+    plt.yticks(fontsize=fontsize)
+    plt.tight_layout()
+    plt.savefig(path)
+    plt.show()
