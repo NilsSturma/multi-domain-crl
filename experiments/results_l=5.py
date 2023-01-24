089fe0f003c80bf73f100271f25ff6ca2f461d3d
Nils Sturma <nils.sturma@web.de>
2023-01-24 14:28:32 +0100

    corrected scale of plots

--- experiments/results_l=5.py
+++ experiments/results_l=5.py
@@ -23,12 +23,12 @@ with open(f"results-paper/nr_doms=4_l={l}_d={d}.pkl", "rb") as f:
 nsamples_list = info2["metadata"]["nsamples_list"]
 avg_number_shared2, too_many_shared_rate2, mixing_error_rate2, graph_error_median2 = get_statistics(info2)
 avg_number_shared3, too_many_shared_rate3, mixing_error_rate3, graph_error_median3 = get_statistics(info3)
-avg_number_shared4, too_many_shared_rate4, mixing_error_rate4, graph_error_median4 = get_statistics(info3)
+avg_number_shared4, too_many_shared_rate4, mixing_error_rate4, graph_error_median4 = get_statistics(info4)
 
 # ===== PLOT =====
 plot(nsamples_list, avg_number_shared2, avg_number_shared3, avg_number_shared4,
                 ylabel="Average of $\hat{\ell}$",
-                path=f"results-paper/avg-shared-nodes_l={l}_d={d}.png", ylim=(-0.1,3.1))
+                path=f"results-paper/avg-shared-nodes_l={l}_d={d}.png", ylim=(-0.1,5.1))
 
 plot(nsamples_list, too_many_shared_rate2, too_many_shared_rate3, too_many_shared_rate4,
                 ylabel="Fraction with $\hat{\ell} > \ell$",
